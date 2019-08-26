a1e155af433557edbe53898f73234a491b51b418
Gonzalo <gonzalo.povea94@gmail.com>
2019-08-26 11:45:15 -0400

    Subida la lista de persona con el servicio de LoggingServices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Persona } from './personas.model';
+import { LoggingService } from './LoggingService.service';
 
 
 @Component({
@@ -11,8 +12,10 @@ export class AppComponent {
   titulo = 'Listado de personas';
   personas: Persona[] = [new Persona("Juan", "Perez"), new Persona("Laura", "America")];
 
+  constructor(private LoggingService :LoggingService){}
 
 onPersonaAgregada(persona:Persona){
+this.LoggingService.enviaMensajeaaConsola("Se agrego la persona "+persona.nombre + " al arreglo"); 
 this.personas.push(persona);
 
 }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 import {Persona} from '../personas.model';
+import { LoggingService } from '../LoggingService.service';
 
 @Component({
   selector: 'app-formulario',
@@ -8,16 +9,18 @@ import {Persona} from '../personas.model';
 })
 export class FormularioComponent implements OnInit {
   @Output() personaCreada = new EventEmitter<Persona>();
-  nombreInput: string;
-  apellidoInput: string;
-  
+  //nombreInput: string;
+  //apellidoInput: string;
+  @ViewChild('nombreInput') nombreInput:ElementRef;
+  @ViewChild('apellidoInput') apellidoInput:ElementRef;
 
-  constructor() { }
+  constructor(private LoggingService:LoggingService) { }
 
   ngOnInit() {
   }
   onAgregarPersonas(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
+    this.LoggingService.enviaMensajeaaConsola("Se envia Persona: "+persona1.nombre + " de apellido " + persona1.apellido);
     this.personaCreada.emit(persona1);
 
   }
